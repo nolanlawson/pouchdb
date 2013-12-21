a8b2248f2803e521f1ab7c7eb278de021a87d7f0
Nolan Lawson <nolan.lawson@gmail.com>
2013-12-20 23:25:19 -0800

    add test that's broken for local but not http

--- tests/test.all_docs.js
+++ tests/test.all_docs.js
@@ -234,6 +234,37 @@ adapters.map(function(adapter) {
     });
   });
 
+  asyncTest('test total_rows with both skip and limit', 6, function() {
+    testUtils.initTestDB(this.name, function(err, db) {
+      var docs = {
+        docs: [
+          {_id: "w", foo: "w"},
+          {_id: "x", foo: "x"},
+          {_id: "y", foo: "y"},
+          {_id: "z", foo: "z"}
+        ]
+      };
+      db.bulkDocs(docs, function(err, res) {
+        db.allDocs({ startkey: 'x', limit: 1 , skip : 1}, function (err, res) {
+          equal(res.total_rows, 4, 'Accurately return total_rows count');
+          equal(res.rows.length, 1, 'Correctly limit the returned rows');
+          equal(res.rows[0].id, 'y', 'Correctly skip 1 doc');
+
+          db.get('x', function(err, xDoc) {
+            db.remove(xDoc, function(err, res){
+              db.allDocs({ startkey: 'w', limit: 2 , skip : 1}, function(err, res) {
+                equal(res.total_rows, 3, 'Accurately return total_rows count after delete');
+                equal(res.rows.length, 2, 'Correctly limit the returned rows after delete');
+                equal(res.rows[0].id, 'y', 'Correctly skip 1 doc after delete');
+                start();
+              })
+            })
+          })
+        });
+      });
+    });
+  });
+
   asyncTest('test limit option and total_rows', function() {
     testUtils.initTestDB(this.name, function(err, db) {
       var docs = {
