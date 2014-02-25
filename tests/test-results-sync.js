ce554edf518330d83ca0741a7cf5ba57ba40476f
Nolan Lawson <nolan.lawson@gmail.com>
2014-02-25 09:21:28 -0800

    more work on the test results sync module

---
+++ tests/test-results-sync.js
@@ -0,0 +1,86 @@
+/* export testResultsSync */
+/* global jQuery,PouchDB */
+
+'use strict';
+
+var testResultsSync;
+
+(function ($) {
+
+  function uuid() {
+    var S4 = function () {
+      return Math.floor(Math.random() * 65536).toString(16);
+    };
+    return S4() + S4() + S4() + S4() + S4() + S4() + S4() + S4();
+  }
+
+  var sessionUsername = 'testuser-' + uuid();
+  var password = uuid();
+  var testResultsDb = new PouchDB(sessionUsername);
+
+  function makeDoc(test) {
+    return {
+      user     : sessionUsername,
+      session  : sessionUsername,
+      async    : test.async,
+      duration : test.duration,
+      pending  : test.pending,
+      parent   : test.parent.fullTitle(),
+      title    : test.title,
+      type     : test.type,
+      speed    : test.speed,
+      state    : test.state,
+      sync     : test.sync,
+      err      : test.err,
+    };
+  }
+
+  var api = {};
+
+  api.onPassOrFail = function (test) {
+    testResultsDb.post(makeDoc(test));
+  };
+
+  // sync test results to server
+  var date = new Date();
+  var userId = 'org.couchdb.user:' + sessionUsername;
+  var sessionAsUser = {
+    // couchdb _user stuff
+    name          : sessionUsername,
+    password      : password,
+    type          : 'user',
+    roles         : [],
+    _id           : userId
+  };
+
+  $.ajax({
+    url : window.COUCHDB_HOST + '/_users/' + userId,
+    method : 'PUT',
+    data   : JSON.stringify(sessionAsUser),
+    contentType: 'application/json',
+    dataType: 'json'
+  }).success(function (res) {
+      var session = {
+        // info to save
+        user          : sessionUsername,
+        browser       : window.jQuery.ua.browser,
+        device        : window.jQuery.ua.device,
+        engine        : window.jQuery.ua.engine,
+        cpu           : window.jQuery.ua.cpu,
+        userAgent     : window.navigator.userAgent,
+        timestamp     : date.toJSON(),
+        time          : date.getTime(),
+        type          : 'session'
+      };
+      testResultsDb.post(session);
+      var testResultsUrl = window.COUCHDB_HOST.replace('/\/\//', '//' + sessionUsername + ':' + password + '@') +
+        '/test_reports';
+      testResultsDb.replicate.to(testResultsUrl, {continuous : true});
+    }).error(function (err) {
+      console.log(err.responseText);
+    });
+
+  testResultsSync = api;
+
+
+})(jQuery);
--- tests/webrunner.js
+++ tests/webrunner.js
@@ -1,4 +1,4 @@
-/* global mocha: true */
+/* global testResultsSync,mocha: true */
 
 'use strict';
 
@@ -8,83 +8,17 @@ var results = {
   failed: 0,
   failures: []
 };
-function uuid () {
-  var S4 = function () {
-    return Math.floor(Math.random() * 65536).toString(16);
-  };
-  return S4() + S4() + S4() + S4() + S4() + S4() + S4() + S4();
-}
-
-var sessionUsername = 'testuser-' + uuid();
-var password = uuid();
-var testResultsDb = new PouchDB(sessionUsername);
-
-function makeDoc(test) {
-  return {
-    user     : sessionUsername,
-    session  : sessionUsername,
-    async    : test.async,
-    duration : test.duration,
-    pending  : test.pending,
-    parent   : test.parent.fullTitle(),
-    title    : test.title,
-    type     : test.type,
-    speed    : test.speed,
-    state    : test.state,
-    sync     : test.sync,
-    err      : test.err,
-    _id      : test.fullTitle()
-  };
-}
 
 runner.on('pass', function (test) {
+  testResultsSync.onPassOrFail(test);
   results.passed++;
-  testResultsDb.put(makeDoc(test));
 });
 
-runner.on('fail', function (test) {
+runner.on('fail', function (e) {
+  testResultsSync.onPassOrFail(e);
   results.failed++;
   results.failures.push({
-    title: test.title,
-    err: test.err
+    title: e.title,
+    err: e.err
   });
-  testResultsDb.put(makeDoc(test));
 });
-
-(function ($) {
-  // sync test results to server
-  var date = new Date();
-  var sessionAsUser = {
-    // couchdb _user stuff
-    name          : sessionUsername,
-    password      : password,
-    type          : 'user',
-    roles         : [],
-    _id           : 'org.couchdb.user:' + sessionUsername
-  };
-
-  $.ajax({
-    url : window.COUCHDB_HOST + '/_users',
-    method : 'PUT',
-    data   : sessionAsUser
-  }).success(function () {
-    var session = {
-      // info to save
-      _id           : sessionUsername,
-      user          : sessionUsername,
-      browser       : window.jQuery.ua.browser,
-      userAgent     : window.navigator.userAgent,
-      timestamp     : date.toJSON(),
-      time          : date.getTime(),
-      type          : 'session'
-    };
-    testResultsDb.put(session);
-    var testResultsUrl = window.COUCHDB_HOST.replace('/\/\//', '//' + sessionUsername + ':' + password + '@') +
-      '/test_results';
-    testResultsDb.replicate.to(testResultsUrl);
-  }).error(function (err) {
-    console.log(err);
-  });
-
-
-})(jQuery);
